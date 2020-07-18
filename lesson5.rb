ff27380c2c2a472a47d92f4b5447a9bf41defdb6
xblueoceanx <ginnnokizuna@yahoo.co.jp>
2020-07-18 18:55:10 +0900

    [Finish]2-6

--- lesson5.rb
+++ lesson5.rb
@@ -1,1 +1,14 @@
 puts 100 + 5
+puts "Rubyを学習"
+name = "鈴木さん"
+puts "Hello" + name
+puts "こんにちは#{name}"
+names = ["Perl", "Ruby", "Python", "Swift"]
+puts names[1]
+tall = {"太郎"=>185, "二郎"=>170, "花子"=>150}
+puts tall["太郎"]
+tall = {:太郎=>185, :二郎=>170, :花子=>150}
+puts tall[:花子]
+
+subjects = [:国語,:算数,:理科,:社会]
+puts subjects[2]
