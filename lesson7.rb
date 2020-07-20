6d5725e5693b2eae34ec8fca7a071de67ef60bd6
xblueoceanx <ginnnokizuna@yahoo.co.jp>
2020-07-20 14:38:29 +0900

    [add]2-8

--- lesson7.rb
+++ lesson7.rb
@@ -1,9 +1,12 @@
-dice = 0
-while dice != 6 do
-	dice = rand(1..6)
-	puts dice
+{"apple"=>130, "strawberry"=>180, "orange"=>100}.each do |fruit, price|
+  puts "#{fruit}は#{price}円です。"
 end
 
-for i in 1..10 do
+i=0
+while i <= 10 do
+	if i>5
+		break
+	end
 	puts i
+	i += 1
 end
